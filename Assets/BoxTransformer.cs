1b808e58f0a1d30a9f3bbf03054c66d48e3690bd
Unknown <ostanin.m.13@gmail.com>
2019-04-04 12:08:39 +0300

    Prefab for box, example for buttons

--- Assets/BoxTransformer.cs
+++ Assets/BoxTransformer.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class BoxTransformer : MonoBehaviour {
 
@@ -7,9 +8,18 @@ public class BoxTransformer : MonoBehaviour {
 	public float z = 0.1f;
 
 	public Transform shape;
+
+    [Tooltip("4 buttons from top in clockwize direction")]
+    public GameObject[] face_buttons = new GameObject[4];
 	
 	[ContextMenu("Fire")]
 	public void Fire(){
 		shape.localScale = new Vector3(x,y,z);
-	}
+
+        face_buttons[0].transform.localPosition = new Vector3(0, y / 2 - 0.01f, -z / 2);
+        face_buttons[1].transform.localPosition = new Vector3(x / 2 - 0.01f, 0, -z / 2);
+        face_buttons[2].transform.localPosition = new Vector3(0, -(y / 2 - 0.01f), -z / 2);
+        face_buttons[3].transform.localPosition = new Vector3(-(x / 2 - 0.01f), 0, -z / 2);
+
+    }
 }
