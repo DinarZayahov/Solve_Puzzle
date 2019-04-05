3f09bd64dff6d525b3a9959432c0162585e93960
DinarZayahov <42997851+DinarZayahov@users.noreply.github.com>
2019-04-05 16:39:01 +0300

    05.04.19

---
+++ Assets/Scripts/BoxTransformer.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class BoxTransformer : MonoBehaviour {
+
+	public float x = 0.1f;
+	public float y = 0.1f;
+	public float z = 0.1f;
+
+	public Transform shape;
+
+    [Tooltip("4 buttons from top in clockwise direction")]
+    public GameObject[] face_buttons = new GameObject[4];
+    public GameObject[] right_buttons = new GameObject[4];
+    public GameObject[] back_buttons = new GameObject[4];
+    public GameObject[] left_buttons = new GameObject[4];
+    public GameObject[] bottom_buttons = new GameObject[4];
+    public GameObject[] top_buttons = new GameObject[4];
+
+    [ContextMenu("Fire")]
+	public void Fire(){
+		shape.localScale = new Vector3(x,y,z);
+
+        face_buttons[0].transform.localPosition = new Vector3(0, y / 2 - 0.01f, -z / 2);
+        face_buttons[1].transform.localPosition = new Vector3(x / 2 - 0.01f, 0, -z / 2);
+        face_buttons[2].transform.localPosition = new Vector3(0, -(y / 2 - 0.01f), -z / 2);
+        face_buttons[3].transform.localPosition = new Vector3(-(x / 2 - 0.01f), 0, -z / 2);
+
+        right_buttons[0].transform.localPosition = new Vector3(x/2, y/2 - 0.01f, 0);
+        right_buttons[1].transform.localPosition = new Vector3(x / 2, 0, z / 2 - 0.01f);
+        right_buttons[2].transform.localPosition = new Vector3(x / 2, -(y / 2 - 0.01f), 0);
+        right_buttons[3].transform.localPosition = new Vector3(x / 2, 0, -(z / 2 - 0.01f));
+
+        back_buttons[0].transform.localPosition = new Vector3(0, y / 2 - 0.01f, z / 2);
+        back_buttons[1].transform.localPosition = new Vector3(-(x / 2 - 0.01f), 0, z / 2);
+        back_buttons[2].transform.localPosition = new Vector3(0, -(y / 2 - 0.01f), z / 2);
+        back_buttons[3].transform.localPosition = new Vector3(x / 2 - 0.01f, 0, z / 2);
+
+        left_buttons[0].transform.localPosition = new Vector3(- x / 2, y / 2 - 0.01f, 0);
+        left_buttons[1].transform.localPosition = new Vector3(- x / 2, 0, -(z / 2 - 0.01f));
+        left_buttons[2].transform.localPosition = new Vector3(- x / 2, -(y / 2 - 0.01f), 0);
+        left_buttons[3].transform.localPosition = new Vector3(- x / 2, 0, z / 2 - 0.01f);
+
+        bottom_buttons[0].transform.localPosition = new Vector3(0, y / 2, z / 2 - 0.01f);
+        bottom_buttons[1].transform.localPosition = new Vector3(x / 2 - 0.01f, y/2, 0);
+        bottom_buttons[2].transform.localPosition = new Vector3(0, y / 2, -(z / 2 - 0.01f));
+        bottom_buttons[3].transform.localPosition = new Vector3(-(x / 2 - 0.01f), y/2, 0);
+
+        top_buttons[0].transform.localPosition = new Vector3(0, - y / 2, -(z / 2 - 0.01f));
+        top_buttons[1].transform.localPosition = new Vector3(x / 2 - 0.01f, -y / 2, 0);
+        top_buttons[2].transform.localPosition = new Vector3(0, - y / 2, z / 2 - 0.01f);
+        top_buttons[3].transform.localPosition = new Vector3(-(x / 2 - 0.01f), - y / 2, 0);
+
+        face_buttons[0].transform.localRotation = Quaternion.Euler(0, 0, 0);
+        face_buttons[1].transform.localRotation = Quaternion.Euler(0, 0, -90);
+        face_buttons[2].transform.localRotation = Quaternion.Euler(0, 0, 180);
+        face_buttons[3].transform.localRotation = Quaternion.Euler(0, 0, 90);
+
+        right_buttons[0].transform.localRotation = Quaternion.Euler(0, -90, 0);
+        right_buttons[1].transform.localRotation = Quaternion.Euler(0, -90, -90);
+        right_buttons[2].transform.localRotation = Quaternion.Euler(0, -90, 180);
+        right_buttons[3].transform.localRotation = Quaternion.Euler(0, -90, 90);
+
+        back_buttons[0].transform.localRotation = Quaternion.Euler(0, 0, 0);
+        back_buttons[1].transform.localRotation = Quaternion.Euler(0, 0, 90);
+        back_buttons[2].transform.localRotation = Quaternion.Euler(0, 0, 180);
+        back_buttons[3].transform.localRotation = Quaternion.Euler(0, 0, -90);
+
+        left_buttons[0].transform.localRotation = Quaternion.Euler(0, 270, 0);
+        left_buttons[1].transform.localRotation = Quaternion.Euler(0, 270, 90);
+        left_buttons[2].transform.localRotation = Quaternion.Euler(0, 270, 180);
+        left_buttons[3].transform.localRotation = Quaternion.Euler(0, 270, -90);
+
+        bottom_buttons[0].transform.localRotation = Quaternion.Euler(90, 0, 0);
+        bottom_buttons[1].transform.localRotation = Quaternion.Euler(-90, -90, 0);
+        bottom_buttons[2].transform.localRotation = Quaternion.Euler(-90, 0, 0);
+        bottom_buttons[3].transform.localRotation = Quaternion.Euler(-90, 90, 0);
+
+        top_buttons[0].transform.localRotation = Quaternion.Euler(-90, 0, 0);
+        top_buttons[1].transform.localRotation = Quaternion.Euler(-90, -90, 0);
+        top_buttons[2].transform.localRotation = Quaternion.Euler(90, 0, 0);
+        top_buttons[3].transform.localRotation = Quaternion.Euler(90, -90, 0);
+    }
+}
--- Assets/Scripts/Button_Receiver.cs
+++ Assets/Scripts/Button_Receiver.cs
@@ -8,28 +8,40 @@ namespace HoloToolkit.Unity.Examples
 {
     public class Button_Receiver : InteractionReceiver
     {
-        public GameObject obj;
-
-        protected override void FocusEnter(GameObject obj, PointerSpecificEventData eventData)
-        {
-            Debug.Log(obj.name + " : FocusEnter");
-        }
-
-        protected override void FocusExit(GameObject obj, PointerSpecificEventData eventData)
-        {
-            Debug.Log(obj.name + " : FocusExit");  
-        }
+        // main object with buttons
+        public GameObject obj; 
 
         protected override void InputDown(GameObject but, InputEventData eventData)
         {
             Debug.Log(but.name + " : InputUp");
             
             int index = 0;
+
+            /* x defines sides of polyhedron:
+            0 is face;
+            1 is right side;
+            2 is back side;
+            3 is left side;
+            4 is bottom;
+            5 is top;
+             */
             int x = 0;
+
+            // getting the index of button
             index = Get_Button_Index(but);
+
+            // dividing index by number of buttons in one side we can determine the side where current button is located
             x = index / 4;
+
+            /* taking the remainder of index we can determine that button:
+             * 0 -> rotates up;
+             * 1 -> rotates right;
+             * 2 -> rotates down;
+             * 3 -> rotates left;
+             */
             index = index % 4;
             
+            // call function according to where we need to rotate
             switch (index)
             {
                 case 0:
@@ -47,10 +59,7 @@ namespace HoloToolkit.Unity.Examples
             }
         }
 
-        protected override void InputUp(GameObject obj, InputEventData eventData)
-        {
-            Debug.Log(obj.name + " : InputUp");
-        }
+       // call functions according to where the button is located
 
         public void Rotate_Up(GameObject obj,int x)
         {
@@ -152,6 +161,7 @@ namespace HoloToolkit.Unity.Examples
             }
         }
 
+        // find and return the index of button in Interactables array
         public int Get_Button_Index(GameObject but)
         {
             for (int i = 0; i < interactables.Capacity; i++)
@@ -162,6 +172,21 @@ namespace HoloToolkit.Unity.Examples
                 }
             }
             return 0;
+        }
+
+        protected override void FocusEnter(GameObject obj, PointerSpecificEventData eventData)
+        {
+            Debug.Log(obj.name + " : FocusEnter");
+        }
+
+        protected override void FocusExit(GameObject obj, PointerSpecificEventData eventData)
+        {
+            Debug.Log(obj.name + " : FocusExit");
+        }
+
+        protected override void InputUp(GameObject obj, InputEventData eventData)
+        {
+            Debug.Log(obj.name + " : InputUp");
         }
     }
 }
