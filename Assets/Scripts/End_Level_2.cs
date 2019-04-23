a293ba5b73d79a39ba2a0e577d37ec83da426d14
Unknown <ostanin.m.13@gmail.com>
2019-04-23 22:26:51 +0300

    Change 2nd lvl

--- Assets/Scripts/End_Level_2.cs
+++ Assets/Scripts/End_Level_2.cs
@@ -36,37 +36,23 @@ public class End_Level_2 : MonoBehaviour {
     }
 
     public void Move() {
-        float x = box.transform.position.x;
-        float y = box.transform.position.y;
-        float z = box.transform.position.z;
-        GameObject face = GameObject.Find("Face");
-        float s = face.transform.localScale.x;
-        
-        objects[0].transform.position = Vector3.MoveTowards(objects[0].transform.position, new Vector3(x - s / 3, y - s / 6, z - s / 6), Time.deltaTime*speed);
-        objects[0].transform.rotation = Quaternion.Euler(0, 0, 90);
 
-        objects[1].transform.position = Vector3.MoveTowards(objects[1].transform.position, new Vector3(x - s / 6, y + s / 6, z + s / 3), Time.deltaTime * speed);
-        objects[1].transform.rotation = Quaternion.Euler(90,0,0);
+        objects[0].transform.position = Vector3.MoveTowards(objects[0].transform.position, new Vector3(-0.3f, 0, 1.8f), Time.deltaTime * speed);
+        objects[0].transform.rotation = Quaternion.Euler(0, -90, 0);
 
-        objects[2].transform.position = Vector3.MoveTowards(objects[2].transform.position, new Vector3(x - s / 6, y + s / 3, z - s / 6), Time.deltaTime * speed);
-        objects[2].transform.rotation = Quaternion.Euler(0, 0, 0);
+        objects[1].transform.position = Vector3.MoveTowards(objects[1].transform.position, new Vector3(-0.3f, 0, 2f), Time.deltaTime * speed);
+        objects[1].transform.rotation = Quaternion.Euler(0, 0, 0);
 
-        objects[3].transform.position = Vector3.MoveTowards(objects[3].transform.position, new Vector3(x + s / 3 + 0.02f, y + s / 6, z - s / 3), Time.deltaTime * speed);
-        objects[3].transform.rotation = Quaternion.Euler(90,0,0);
+        objects[2].transform.position = Vector3.MoveTowards(objects[2].transform.position, new Vector3(-0.1f, 0, 2f), Time.deltaTime * speed);
+        objects[2].transform.rotation = Quaternion.Euler(-90, -180, 90);
 
-        objects[4].transform.position = Vector3.MoveTowards(objects[4].transform.position, new Vector3(x + s / 3, y + s / 3, z + s / 3), Time.deltaTime * speed);
-        objects[4].transform.rotation = Quaternion.Euler(0, 0, 0);
+        objects[3].transform.position = Vector3.MoveTowards(objects[3].transform.position, new Vector3(-0.5f, 0.2f, 1.8f), Time.deltaTime * speed);
+        objects[3].transform.rotation = Quaternion.Euler(0, 0, 0);
 
-        objects[5].transform.position = Vector3.MoveTowards(objects[5].transform.position, new Vector3(x + s / 3, y + s / 3, z), Time.deltaTime * speed);
+        objects[4].transform.position = Vector3.MoveTowards(objects[4].transform.position, new Vector3(-0.2f, 0.2f, 1.8f), Time.deltaTime * speed);
+        objects[4].transform.rotation = Quaternion.Euler(0, 90, 0);
+
+        objects[5].transform.position = Vector3.MoveTowards(objects[5].transform.position, new Vector3(-0.4f, 0.2f, 2.1f), Time.deltaTime * speed);
         objects[5].transform.rotation = Quaternion.Euler(0, 0, 0);
-
-        objects[6].transform.position = Vector3.MoveTowards(objects[6].transform.position, new Vector3(x + s / 3, y - s / 3, z-0.03f), Time.deltaTime * speed);
-        objects[6].transform.rotation = Quaternion.Euler(0,180,0);
-
-        objects[7].transform.position = Vector3.MoveTowards(objects[7].transform.position, new Vector3(x, y - s / 3, z), Time.deltaTime * speed);
-        objects[7].transform.rotation = Quaternion.Euler(0, 0, 0);
-
-        objects[8].transform.position = Vector3.MoveTowards(objects[8].transform.position, new Vector3(x + s / 3, y, z), Time.deltaTime * speed);
-        objects[8].transform.rotation = Quaternion.Euler(180,90,0);
     }
 }
--- Assets/Scripts/Help_2nd_level.cs
+++ Assets/Scripts/Help_2nd_level.cs
@@ -39,33 +39,13 @@ public class Help_2nd_level : MonoBehaviour
 
     public void Move()
     {
-        float x = box.transform.position.x;
-        float y = box.transform.position.y;
-        float z = box.transform.position.z;
-        GameObject face = GameObject.Find("Face");
-        float s = face.transform.localScale.x;
+        objects[0].transform.position = Vector3.MoveTowards(objects[0].transform.position, new Vector3(-0.3f, 0, 1.8f), Time.deltaTime * speed);
+        objects[0].transform.rotation = Quaternion.Euler(0, -90, 0);
 
-        objects[2].transform.position = Vector3.MoveTowards(objects[2].transform.position, new Vector3(x - s / 3, y - s / 6, z - s / 6), Time.deltaTime * speed);
-        objects[2].transform.rotation = Quaternion.Euler(0, 0, 90);
+        objects[1].transform.position = Vector3.MoveTowards(objects[1].transform.position, new Vector3(-0.3f, 0, 2f), Time.deltaTime * speed);
+        objects[1].transform.rotation = Quaternion.Euler(0, 0, 0);
 
-        objects[4].transform.position = Vector3.MoveTowards(objects[4].transform.position, new Vector3(x - s / 6, y + s / 6, z + s / 3), Time.deltaTime * speed);
-        objects[4].transform.rotation = Quaternion.Euler(90, 0, 0);
-
-        /**objects[2].transform.position = Vector3.MoveTowards(objects[2].transform.position, new Vector3(x - s / 6, y + s / 3, z - s / 6), Time.deltaTime * speed);
-
-        objects[3].transform.position = Vector3.MoveTowards(objects[3].transform.position, new Vector3(x + s / 3, y + s / 6, z - s / 3), Time.deltaTime * speed);
-        objects[3].transform.rotation = Quaternion.Euler(90, 0, 0);
-
-        objects[4].transform.position = Vector3.MoveTowards(objects[4].transform.position, new Vector3(x + s / 3, y + s / 3, z + s / 3), Time.deltaTime * speed);
-
-        objects[5].transform.position = Vector3.MoveTowards(objects[5].transform.position, new Vector3(x + s / 3, y + s / 3, z), Time.deltaTime * speed);*/
-
-        objects[1].transform.position = Vector3.MoveTowards(objects[1].transform.position, new Vector3(x + s / 3, y - s / 3, z), Time.deltaTime * speed);
-        objects[1].transform.rotation = Quaternion.Euler(0, 180, 0);
-
-        objects[0].transform.position = Vector3.MoveTowards(objects[0].transform.position, new Vector3(x, y - s / 3, z), Time.deltaTime * speed);
-
-        objects[3].transform.position = Vector3.MoveTowards(objects[3].transform.position, new Vector3(x + s / 3, y, z), Time.deltaTime * speed);
-        objects[3].transform.rotation = Quaternion.Euler(180, 90, 0);
+        objects[2].transform.position = Vector3.MoveTowards(objects[2].transform.position, new Vector3(-0.1f, 0, 2f), Time.deltaTime * speed);
+        objects[2].transform.rotation = Quaternion.Euler(-90, -180, 90);
     }
 }
